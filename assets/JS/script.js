eebeec139be664e29e761fa36d7626d8e4e1f4ef
Gaelle Gaban <gaellegaban@gmail.com>
2019-02-20 12:18:15 +0100

    troisieme jour

---
+++ assets/JS/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    if($('body').is('.accueil')){
+        //var person = prompt("Quel est ton prénom?");
+        if (person != null) {
+          document.getElementById("prenom").innerHTML = person;
+        } 
+    }
+});
+
+
